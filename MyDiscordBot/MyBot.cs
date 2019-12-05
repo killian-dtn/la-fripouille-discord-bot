96cb4f25b02f770a79abc907ec3c2257e501d403
killian-dtn <killian.dantansentr@gmail.com>
2019-12-05 21:19:50 +0100

    Add src

---
+++ MyDiscordBot/MyBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordBotBase;
+
+namespace MyDiscordBot
+{
+    public class MyBot : BasicBot
+    {
+        public MyBot(string token) : base(token) { }
+    }
+}
---
+++ MyDiscordBot/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace MyDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new MyBot(ConfigurationManager.AppSettings["Token"]).StartAsync().GetAwaiter().GetResult();
+    }
+}
